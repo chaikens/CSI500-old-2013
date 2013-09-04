11e67792040ef2115a1996c6622f697cd704177c
Seth Chaiken <sdc@cs.albany.edu>
2013-09-04 08:17:23 -0400

    got it to read and recognize a command.

--- Proj01/interact.c
+++ Proj01/interact.c
@@ -2,6 +2,13 @@
 
 int main(int argc, char *arg[])
 {
+  char *pToInputLine;
+  int nread;
   printf("Hello. This is an interactive program.\n");
-  return 0;
+  nread = getline( &pToInputLine, &nread, stdin);
+  printf("Thanks for %s A line of length %d\n", pToInputLine, nread);
+  if( strcmp(pToInputLine, "FirstCommand\n")==0)
+    {
+      printf("Thanks for that FirstCommand!\n");
+    }
 }
