642a85421aacb01ce2a343ea3ccdfc00f5e25baa
Seth Chaiken <sdc@cs.albany.edu>
2013-09-22 20:32:04 -0400

    First successful compile. Doesn't recongize admin, I think I must
    put newlines in the literal strings to compare.

---
+++ Proj01/medoffice.c
@@ -0,0 +1,77 @@
+#include "csapp.h"
+
+struct command_struct {
+  char *role;
+  char *choice;
+  char *command;
+};
+
+static struct command_struct ARRAY[ ] =
+  {
+    {
+      .role = "Doctor",
+      .choice = "d",
+      .command = "./doctor"
+    },
+    {
+      .role = "Nurse",
+      .choice = "n",
+      .command = "./nurse"
+    },
+    {
+      .role = "Receptionist",
+      .choice = "r",
+      .command = "./receptionist"
+    }
+  };
+
+
+static void printmenu()
+{
+  int nchoices =  sizeof( ARRAY )/sizeof( struct command_struct );
+  printf("Menu stub\n");
+  printf("Number of choices is %d\n", nchoices );
+}
+
+static char * getappfile(char *choice )
+{
+  printf("getappfile stub\n");
+  return 0;
+}
+
+
+static void docommandwaiting( char * command )
+{
+  printf("docommandwaiting stub\n");
+}
+
+int main(int argc, char *argv[])
+{
+  char *inbuffer = 0; /* Must initialize so getline allocates the buffer for us. */
+  size_t bufsize; /* Initialization not needed since getline will ignore when inbuffer==0. */
+  int looping = 1;
+
+  while( looping ) {
+    printmenu( );
+    printf("Type admin to administer\n");
+    getline( &inbuffer, &bufsize, stdin);
+    if( strcmp(inbuffer,"admin") == 0 ) { 
+      printf("Your password please:");
+      getline( &inbuffer, &bufsize, stdin);
+      if( strcmp(inbuffer,"IAmTheBoss") == 0 ) {
+	looping = 0;
+      } else {
+	char * command = getappfile( inbuffer );
+	if( command == NULL ) {
+	  printf("Unrecognized office role.\n");
+	}
+	else {
+	  docommandwaiting( command );
+	}
+      }
+    }
+  }
+
+  printf("If you see this, either you're an admin or you broke into our system.\n");
+  return 0;
+}
---
+++ Proj01/nurse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  char *inbuffer = 0; /* Must initialize so getline allocates the buffer for us. */
+  size_t bufsize; /* Initialization not needed since getline will ignore when inbuffer==0. */
+  printf("Hello nurse.  This app is a stub. Press enter to exit.");
+  getline( &inbuffer, &bufsize, stdin );
+  return 0;
+}
